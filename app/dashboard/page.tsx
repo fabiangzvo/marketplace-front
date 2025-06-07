0979ef6179c90843155815c9bf76126e7d76405b
fabiangzvo <faguzman.97@gmail.com>
2025-06-06 20:32:32 -0500

    add dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { JSX } from "react";
+import { Button } from "@heroui/button";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
-  return <div>page</div>;
+import { getSession } from "@/lib/session";
+
+export default async function DashboardPage(): Promise<JSX.Element> {
+  const session = await getSession();
+
+  if (!session) redirect("/");
+
+  return (
+    <div className="min-h-[70vh]">
+      <div className="flex justify-between">
+        <h1 className="text-lg font-bold mb-6">
+          Hola {session?.user.name ?? session?.user?.email ?? "👋"}
+        </h1>
+        <Button as={Link} color="primary" href="/product/create">
+          Crear Producto
+        </Button>
+      </div>
+    </div>
+  );
 }
